f034bfd5f461301af22af6cbaca8c68ec5959698
NicholasTourony <natourony@quinnipiac.edu>
2024-03-05 15:45:44 -0500

    give user a DMV number and cycle to call them

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,7 +1,22 @@
+import java.util.Random;
+
 public class DmvSimulator
 {
     public static void main(String[] args)
     {
-        
+        System.out.println("Welcome to the DMV!");
+        Random random = new Random();
+        int userNumber = random.nextInt(1, 101);
+        System.out.println("You're number " + userNumber + " please wait until you are called");
+        int currentNumber = (userNumber % 100) + 1;
+        boolean notCalled = true;
+        while (notCalled)
+        {
+            System.out.println("Now helping number " + currentNumber);
+            notCalled = !(currentNumber == userNumber);
+            currentNumber = (currentNumber % 100) + 1;
+        }
+        System.out.println("Sorry you don't have the required paperwork");
+
     }
 }

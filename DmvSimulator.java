e34fc00f71ac8886005acd889fc7f52ca2758076
NicholasTourony <natourony@quinnipiac.edu>
2024-03-05 15:53:40 -0500

    increased range of numbers

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -6,15 +6,15 @@ public class DmvSimulator
     {
         System.out.println("Welcome to the DMV!");
         Random random = new Random();
-        int userNumber = random.nextInt(1, 101);
+        int userNumber = random.nextInt(1, 201);
         System.out.println("You're number " + userNumber + " please wait until you are called");
-        int currentNumber = (userNumber % 100) + 1;
+        int currentNumber = (userNumber % 200) + 1;
         boolean notCalled = true;
         while (notCalled)
         {
             System.out.println("Now helping number " + currentNumber);
             notCalled = !(currentNumber == userNumber);
-            currentNumber = (currentNumber % 100) + 1;
+            currentNumber = (currentNumber % 200) + 1;
         }
         System.out.println("Sorry you don't have the required paperwork");
 
